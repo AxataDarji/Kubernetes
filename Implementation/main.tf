196c019390e5f2987d9b8b7292afd341f864734e
Axata Darji <ax583820@dal.ca>
2024-07-08 13:11:13 -0300

    Updated the comment with container details

--- Implementation/main.tf
+++ Implementation/main.tf
@@ -8,10 +8,11 @@ resource "google_container_cluster" "my_cluster" {
   name     = "k8s-gke-cluster"
   location = "us-central1-c"
   enable_autopilot=false
+  initial_node_count=1
 
 
   node_config {
-    machine_type = "e2-micro"       #“e2-micro (2 vCPU, 1GB memory) for Machine type.
+    machine_type = "e2-small"       #“e2-micro (2 vCPU, 1GB memory) for Machine type.
     disk_type = "pd-standard"       #“Standard persistent disk” for Boot disk type.
     disk_size_gb = 10               #“10” for Boot disk size.
     image_type = "COS_CONTAINERD"   #“container-Optimized OS with containerd (cos_containerd) (default)” as the image type for the node.
